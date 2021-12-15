4a6d8122dd090fad54a7e1e4494d0411fb597aa8
chunkie <gsc0518@outlook.com>
2021-12-15 12:09:08 +0800

    NONE

--- src/main/java/com/chunkie/gdy/controller/RoomController.java
+++ src/main/java/com/chunkie/gdy/controller/RoomController.java
@@ -1,20 +1,11 @@
 package com.chunkie.gdy.controller;
 
-import com.chunkie.gdy.common.Constants;
-import com.chunkie.gdy.common.ResponseObj;
-import com.chunkie.gdy.entity.Card;
 import com.chunkie.gdy.entity.Room;
-import com.chunkie.gdy.service.CardService;
-import com.chunkie.gdy.service.PlayerService;
-import com.chunkie.gdy.service.RoomService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
 
 /**
  * @Description:
@@ -28,18 +19,9 @@ import java.util.UUID;
 @RequestMapping("/room")
 public class RoomController {
 
-    @Autowired
-    CardService cardService;
-
-    @Autowired
-    PlayerService playerService;
-
-    @Autowired
-    RoomService roomService;
-
     @RequestMapping("/create")
     public void createRoom(@RequestBody Room room){
-        room.setId(UUID.randomUUID().toString());
+
     }
 
     @RequestMapping("/join")
