9953109d3dde316bde3849581e7eedb5754d7445
chunkie <gsc0518@outlook.com>
2022-01-13 16:24:58 +0800

    13/01/2022

---
+++ src/main/java/com/chunkie/gdy/service/PlayerService.java
@@ -0,0 +1,11 @@
+package com.chunkie.gdy.service;
+
+/**
+ * @Description:
+ * @ClassName: PlayerService
+ * @Author: SichengGuo
+ * @Date: 2022/1/13 16:24
+ * @Version: 1.0
+ */
+public class PlayerService {
+}
